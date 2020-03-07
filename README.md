a2257f22d0cd1b4699b7fc0571fc0cf207718968
rajmayank <mayank9856@gmail.com>
2020-03-07 11:27:53 +0530

    Initialized

---
+++ README.md
@@ -0,0 +1,1 @@
+### AyushNathani.com
---
+++ assets/index.js
@@ -0,0 +1,22 @@
+// JavaScript
+import './scripts/main'
+
+// Styles
+import './scss/main.scss'
+
+// Favicon
+// import './favicon.ico'
+
+// robots.txt
+import './robots.txt'
+
+
+// images
+import './images/img_1.jpg'
+import './images/img_2.jpg'
+import './images/img_3.jpg'
+import './images/img_4.jpg'
+import './images/img_5.jpg'
+import './images/img_6.jpg'
+import './images/img_7.jpg'
+import './images/img_8.jpg'
